f59209b7825636901d1c037680f04fe820e9127d
tan-frederic <tanfrederic33@gmail.com>
2026-02-07 21:06:49 +0100

    Adds data models for the API
    Defines the data models for the API, including:
    
    - RealEstateAsset: Represents a real estate property.
    - Lease: Represents a lease agreement for a property.
    - LeaseDocuments: Represents documents associated with a lease.
    - Tenant: Represents a tenant associated with a lease.
    
    These models will be used for data storage and retrieval in the API.

---
+++ LocaCraftAPI/LocaCraftAPI/Models/Lease.cs
@@ -0,0 +1,20 @@
+﻿namespace LocaCraftAPI.Models
+{
+    public class Lease
+    {
+        public int Id { get; set; }
+        public int RealEstateAssetId { get; set; }
+        public RealEstateAsset? RealEstateAsset { get; set; }
+        public string LeaseeName { get; set; } = string.Empty;
+        public decimal MonthlyRent { get; set; }
+        public decimal MonthlyCharges { get; set; }
+        public decimal Deposit { get; set; }
+
+        public List<Tenant> Tenants { get; set; } = new List<Tenant>();
+        public List<LeaseDocuments> LeaseDocuments { get; set; } = new List<LeaseDocuments>();
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
---
+++ LocaCraftAPI/LocaCraftAPI/Models/RealEstateAsset.cs
@@ -0,0 +1,15 @@
+﻿namespace LocaCraftAPI.Models
+{
+    public class RealEstateAsset
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string AddressComplement { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public List<Lease> Leases { get; set; } = new List<Lease>();
+    }
+}
