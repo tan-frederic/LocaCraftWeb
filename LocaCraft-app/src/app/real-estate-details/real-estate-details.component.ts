713720202b9e704f07af00bcb1914d8cd976b8d6
tan-frederic <tanfrederic33@gmail.com>
2026-02-17 22:07:57 +0100

    Adds real estate details component
    Adds the real estate details component to display detailed information about a specific real estate property.

---
+++ LocaCraft-app/src/app/real-estate-details/real-estate-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-real-estate-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './real-estate-details.component.html',
+  styleUrl: './real-estate-details.component.css'
+})
+export class RealEstateDetailsComponent {
+
+}
