3ec318fd449f80ad38a6b356557dfcb802ade07f
tan-frederic <tanfrederic33@gmail.com>
2026-02-21 11:33:14 +0100

    Adds tenant form component
    Initializes the tenant form component, providing a basic structure for managing tenant-related information.

---
+++ LocaCraft-app/src/app/tenant-form/tenant-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tenant-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './tenant-form.component.html',
+  styleUrl: './tenant-form.component.css'
+})
+export class TenantFormComponent {
+
+}
