efa9703b9ca537867748a1a5491db3c00c99739d
tan-frederic <tanfrederic33@gmail.com>
2026-03-07 10:15:10 +0100

    feat: create user account settings component
    Initializes the standalone component and boilerplate files for the user account settings page.

---
+++ LocaCraft-app/src/app/user-account-settings/user-account-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-account-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-account-settings.component.html',
+  styleUrl: './user-account-settings.component.css'
+})
+export class UserAccountSettingsComponent {
+
+}
